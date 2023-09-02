b98364a8cb61db3c79c026051619f913ed2000b5
tangchunguang <394266315@qq.com>
2023-09-02 22:34:22 +0800

    no message

---
+++ ServerHotfix/AppStart_Init_Custom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace ET
+{
+
+    public class AppStart_Init_Custom : AEvent<EventType.AppStart>
+    {
+        protected override void Run(EventType.AppStart args)
+        {
+            Log.Console("AppStart_Init_Custom");
+            ComHelp.AccountOldLogic = true;
+        }
+    }
+}
