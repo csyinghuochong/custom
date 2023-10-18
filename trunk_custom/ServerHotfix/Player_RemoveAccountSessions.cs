f8c523e056a615e1f9c409f61acb248095aadee0
tangchunguang <394266315@qq.com>
2023-10-18 17:55:03 +0800

    no message

---
+++ trunk_custom/ServerHotfix/Player_RemoveAccountSessions.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace ET
+{
+
+    public class Player_RemoveAccountSessions : AEvent<EventType.RemoveAccountSessions>
+    {
+        protected override void Run(EventType.RemoveAccountSessions args)
+        {
+            Scene scene = args.DomainScene;
+            scene.GetComponent<AccountSessionsComponent>().Remove(args.AccountId);
+        }
+    }
+}
