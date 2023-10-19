cb5e43b3bc6edd58808091881e1f20b02f8f47cd
tangchunguang <394266315@qq.com>
2023-10-19 20:10:38 +0800

    no message

--- trunk_custom/ServerHotfix/Player_RemoveAccountSessions.cs
+++ trunk_custom/ServerHotfix/Player_RemoveAccountSessions.cs
@@ -3,6 +3,27 @@
 namespace ET
 {
 
+    public static class AccountSessionsComponentSystem2
+    {
+        /// <summary>
+        /// AccountCheckOutTimeComponent 十分钟后
+        /// DisconnectHelper.KickPlayer
+        /// G2A_ExitGame
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="accountId"></param>
+        public static void Remove(this AccountSessionsComponent self, long accountId)
+        {
+            if (self.AccountSessionsDictionary.ContainsKey(accountId))
+            {
+                Log.Console($"Remove: {accountId}");
+
+                self.AccountSessionsDictionary.Remove(accountId);
+            }
+        }
+    }
+
+    
     public class Player_RemoveAccountSessions : AEvent<EventType.RemoveAccountSessions>
     {
         protected override void Run(EventType.RemoveAccountSessions args)
