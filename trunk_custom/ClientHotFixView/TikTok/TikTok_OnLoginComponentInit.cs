611d9c84f5327f1c5fc02aa50ec6e0790c20bd96
tangchunguang2 <2322657661@qq.com>
2023-11-20 14:09:19 +0800

    no message

---
+++ trunk_custom/ClientHotFixView/TikTok/TikTok_OnLoginComponentInit.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+namespace ET
+{
+
+    public static class UILoginComponentSystem2
+    {
+
+
+        
+    }
+
+    [Event]
+    public class TikTok_TikTokGetAccesstoken : AEventClass<EventType.TikTokGetAccesstoken>
+    {
+        protected override void Run(object a)
+        {
+            EventType.TikTokGetAccesstoken args = a as EventType.TikTokGetAccesstoken;
+
+            Log.ILog.Debug("GetTiktokAccesstoken: ");
+
+            GameObject.Find("Global").GetComponent<Init>().OnTikTokAccesstokenHandler = args.AccesstokenHandler;
+
+            Init init = GameObject.Find("Global").GetComponent<Init>();
+            init.TikTokLogin();
+        }
+    }
+
+    [Event]
+    public class TikTok_OnRiskControlInfo : AEventClass<EventType.TikTokRiskControlInfo>
+    {
+        protected override void Run(object a)
+        {
+            EventType.TikTokRiskControlInfo args = a as EventType.TikTokRiskControlInfo;
+
+            Log.ILog.Debug("uilogincomponent.OnTikTokAccesstokenHandler");
+            GameObject.Find("Global").GetComponent<Init>().OnTikTokAccesstokenHandler = args.RiskControlInfoHandler;
+
+            GameObject.Find("Global").GetComponent<Init>().TikTokRiskControlInfo();
+        }
+    }
+
+    [Event]
+    public class TikTok_OnTikTokPayRequest : AEventClass<EventType.TikTokPayRequest>
+    {
+        protected override void Run(object a)
+        {
+            EventType.TikTokPayRequest args = a as EventType.TikTokPayRequest;
+
+            GameObject.Find("Global").GetComponent<Init>().TikTokPay(args.PayMessage);
+        }
+    }
+
+
+}
