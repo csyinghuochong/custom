4ca76be14f2d64ef2fdb2220b671676485a32652
tangchunguang <394266315@qq.com>
2024-01-11 10:22:23 +0800

    no message

--- trunk_custom/ClientHotFixView/AppStart_Init_Custom.cs
+++ trunk_custom/ClientHotFixView/AppStart_Init_Custom.cs
@@ -14,11 +14,16 @@ namespace ET
             //PacketParser.OuterPacketSizeLengthC = packetSize;
             ///技能按钮事件
 
-            Vector2 dest = new Vector2(6, 8);
-            Vector2 todo = ShaderMathHelper.GetNormalized(dest);
+         
+            //Vector2 todo = ShaderMathHelper.GetNormalized(dest);
 
-            Log.ILog.Debug($"矢量长度  : {dest.magnitude}   {Math.Sqrt(dest.x * dest.x + dest.y * dest.y)}");
-            Log.ILog.Debug($"矢量归一化: {todo}");
+            //Log.ILog.Debug($"矢量长度  : {dest.magnitude}   {Math.Sqrt(dest.x * dest.x + dest.y * dest.y)}");
+            //Log.ILog.Debug($"矢量归一化: {todo}");
+
+            Vector3 from  = new Vector3(0, 1, 0);
+            Vector3 dest = new Vector3(1, 0, 0);
+            Log.ILog.Debug($"叉乘:  { ShaderMathHelper.GetCross_1(from, dest) }");
+            Log.ILog.Debug($"叉乘:  {ShaderMathHelper.GetCross_2(from, dest)}");
         }
 
     }
--- trunk_custom/ClientHotFixView/ShaderMathHelper.cs
+++ trunk_custom/ClientHotFixView/ShaderMathHelper.cs
@@ -42,12 +42,24 @@ namespace ET
         //矢量的乘法有点积(内积) 和叉积(外积) 
         //点积的名称a.b  公式一： a.b=(ax,ay,az).(bx,by.bz) = ax*bx + ay*by + az*bz 各分量的乘 之和。点积满足交换律
         //点积的一个重要几何意义就是投影
-        public static Vector2 GetDot(Vector2 from, Vector2 dest)
+        public static float GetDot_1(Vector2 from, Vector2 dest)
         {
-            Vector2 vector2 = new Vector2( from.x * dest.x, from.y * dest.y  );
-            return vector2; 
+            float dot = from.x * dest.x +  from.y* dest.y;
+            return dot; 
         }
 
+        public static float GetDot_2(Vector2 from, Vector2 dest)
+        {
+            return Vector2.Dot(from, dest); 
+        }
+
+        public static float GetDot_3(Vector2 from, Vector2 dest)
+        {
+            float angle = Vector2.Angle(from, dest);
+            float hudu = MathHelper.DegToRad(angle);
+            float cos = Mathf.Cos(hudu);
+            return from.magnitude * dest.magnitude * cos;
+        }
         //假设有一个单位矢量a 和 一个长度不限的矢量B.  我们希望得到b 在平行a的一条直线上的投影，  就可以使用单位矢量a . b 得到b 在 a方向上的投影
         //通俗点解释就是 一个光源垂直于a 方向的， b在a方向的投影就是 b 在 a方向上的影子
         //a b的方向夹角大于90度， 结果小于0    等于90度结果小于0   小于90度结果大于0
@@ -58,8 +70,35 @@ namespace ET
         //点积的公式2  a.b = |a||b|cosQ   
         //对两个单位矢量进行点积，a.b    cosQ =直角边/斜边  单位矢量a.b刚好对应cosQ对应的直角边  a.b = 直角边/斜边 = cosQ
         //a.b = (|a|a单位矢量)(|b|b单位矢量) = |a||b|cosQ
-        //两个矢量的点积可以表示为两个矢量的模想乘 再 乘以他们夹角的余铉值 当夹角小于90度 cosQ>0   。夹角=90度 cosQ=0
+        //两个矢量的点积可以表示为两个矢量的模相乘 再 乘以他们夹角的余铉值 当夹角小于90度 cosQ>0   。夹角=90度 cosQ=0
         //利用这个公式我们还可以求得两个向量之间的夹角 = arcos(a.b). 反余弦操作
+
+        //另一个重要的矢量运算就是叉积，也被称为外积。与点积不同的是。矢量叉积的结果任是一个矢量。而非标量a X b
+        //即结果矢量的第一个分量。他是第一个矢量的y分量乘以第二个矢量的z分量 减去 第一个矢量的z分量，乘以第二个矢量的y分量
+        // a X b = ( Ax, Ay, Az ) X (Bx, By, Bz) = ( Ay*Bz - Az*By, Az*Bx - Ax*Bz, Ax*By - Ay*Bx ) 第一个矢量的分量*第二个矢量的对角的分量-第一个矢量的分量（下一个并且非当前）*第二个矢量的对角的分量
+        //叉积不满足交换律，但是满足反交换律 aXb= -(bXa) 也不满足结合律
+        //aXb的长度等于a和b的模再乘以他们之间的正弦值 |aXb| = |a||b|sinQ    a b平行得到的是零向量而非标量0
+        //矢量的方向， 手指的方向a. 手臂的反方向b， 大拇指就是a X b 的方向。  类似于竖起大拇指
+        //叉积的作用是计算垂直于一个平面的三角形的矢量 或者 判断三角片面的朝向   左右手坐标系得到的叉积结果一致   只是看起来相反 细想细想
+        //叉乘大于0在右手边 小于0在左手边
+        public static Vector3 GetCross_1(Vector3 from, Vector3 dest)
+        { 
+            return Vector3.Cross(from, dest);
+        }
+
+        public static Vector3 GetCross_2(Vector3 from, Vector3 dest)
+        {
+            return new Vector3( from.y * dest.z - from.z * dest.y, from.z * dest.x - from.x * dest.z, from.x* dest.y - from.y * dest.x );
+        }
+
+        //根据左手定则，y轴是法向量的方向，当y大于零的时候，敌人在我方右边，当y小于零的时候，敌人在我方左边。
+        public static void CalculateCross(Unit cubeRed, Unit cubeBlue)
+        {
+            Vector3 relativePosition = cubeRed.Position - cubeBlue.Position;
+            Vector3 cubeForward = cubeBlue.Forward;
+            Vector3 result = Vector3.Cross(cubeForward, relativePosition);
+            Log.ILog.Debug(result.y.ToString());
+        }
     }
 }
 
