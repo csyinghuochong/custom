1f22b778ae0f6b95f6d3e4d6727d3d24545a2bd7
tangchunguang <394266315@qq.com>
2024-04-18 12:43:31 +0800

    no message

--- trunk_custom/onedelete.bat
+++ trunk_custom/onedelete.bat
@@ -6,5 +6,10 @@ del/s /q "H:\GitWeiJing\Unity\Assets\Editor\Custom\*.*"
 
 del/s /q "H:\GitWeiJing\Unity\Assets\HotfixView\Custom\*.*"
 
+del/s /q "H:\GitWeiJing\Unity\Assets\Bundles\Text\ETKey.txt"
+
+del/s /q "H:\GitWeiJing\Unity\Assets\Mono\MonoBehaviour\InitHelper.cs"
+
+del/s /q "H:\GitWeiJing\Robot\Hotfix\Robot\*.*"
 
 del/s /q "F:\gitcustom\*.*"
