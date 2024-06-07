3514cd6ca027a2723d187472596eee7c56d07d18
tangchunguang <394266315@qq.com>
2024-06-07 19:04:18 +0800

    no message

--- trunk_custom/dll_local/start_merge_zone.bat
+++ trunk_custom/dll_local/start_merge_zone.bat
@@ -1,4 +1,4 @@
 ::先备份数据库     要移除的区->目标区     在修改物理区
-cd bin
-dotnet Server.dll --AppType=MergeZone --Console=1 --StartConfig=StartConfig/Localhost --Title=MergeZone --Parameters=39_38 --Process 1
+cd binET
+dotnet Server.dll --AppType=MergeZone --Console=1 --StartConfig=StartConfig/Localhost --Title=MergeZone --Parameters=95_93 --Process 1
 pause
