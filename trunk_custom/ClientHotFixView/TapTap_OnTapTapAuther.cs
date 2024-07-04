1ee2567a3434f889da6779a4e0b989e9a89fd873
tangchunguang <394266315@qq.com>
2024-07-04 22:34:09 +0800

    no message

---
+++ trunk_custom/ClientHotFixView/TapTap_OnTapTapAuther.cs
@@ -0,0 +1,13 @@
+
+namespace ET
+{
+    public class TapTap_OnTapTapAuther : AEventClass<EventType.TapTapAuther>
+    {
+        protected override void Run(object numerice)
+        {
+            EventType.TapTapAuther args = numerice as EventType.TapTapAuther;
+
+            
+        }
+    }
+}
