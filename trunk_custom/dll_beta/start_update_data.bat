bf56fc659f7ea542b3d361414e784e2792351575
tangchunguang2 <2322657661@qq.com>
2024-07-04 22:36:25 +0800

    no message

--- trunk_custom/dll_beta/start_update_data.bat
+++ trunk_custom/dll_beta/start_update_data.bat
@@ -1,4 +1,4 @@
 ﻿::先备份数据库     要移除的区—>新区     在修改物理区
 cd publish
-dotnet Server.dll --AppType=UpdateDB --Console=1 --StartConfig=StartConfig/Beta --Title=MergeZone --Process 1
+dotnet Server.dll --AppType=UpdateDB --Console=1 --StartConfig=StartConfig/Beta --Title=UpdateDB --Process 1
 pause
