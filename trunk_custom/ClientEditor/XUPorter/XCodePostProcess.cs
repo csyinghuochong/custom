c266c66c92a90ed92291fd33ca1627e84e8ab3ed
tangchunguang <394266315@qq.com>
2024-09-24 19:00:47 +0800

    no message

--- trunk_custom/ClientEditor/XUPorter/XCodePostProcess.cs
+++ trunk_custom/ClientEditor/XUPorter/XCodePostProcess.cs
@@ -89,6 +89,24 @@ public static class XCodePostProcess
 			Debug.LogWarning("Target is not iPhone. XCodePostProcess will not run");
 			return;
 		}
+
+
+		 var projectPath = PBXProject.GetPBXProjectPath(path);
+        
+        // Adds entitlement depending on the Unity version used
+#if UNITY_2019_3_OR_NEWER
+            var project = new PBXProject();
+            project.ReadFromString(System.IO.File.ReadAllText(projectPath));
+            var manager = new ProjectCapabilityManager(projectPath, "Entitlements.entitlements", null, project.GetUnityMainTargetGuid());
+            manager.AddSignInWithAppleWithCompatibility(project.GetUnityFrameworkTargetGuid());
+            manager.WriteToFile();
+#else
+            var manager = new ProjectCapabilityManager(projectPath, "Entitlements.entitlements", PBXProject.GetUnityTargetName());
+            manager.AddSignInWithAppleWithCompatibility();
+            manager.WriteToFile();
+#endif
+
+
 		UnityEngine.Debug.Log("PostProcess_1: " + pathToBuiltProject);
 		// Create a new project object from build target
 
