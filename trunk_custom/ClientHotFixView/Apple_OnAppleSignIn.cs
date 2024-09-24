b58eb40e436f1eaa3a3ce96c705b5ed1200b64e4
tangchunguang <394266315@qq.com>
2024-09-24 18:00:03 +0800

    no message

---
+++ trunk_custom/ClientHotFixView/Apple_OnAppleSignIn.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using UnityEngine;
+using AppleAuth;
+using AppleAuth.Enums;
+
+namespace ET
+{
+    public class Apple_OnAppleSignIn : AEventClass<EventType.AppleSignIn>
+    {
+        protected override void Run(object numerice)
+        {
+            EventType.AppleSignIn args = numerice as EventType.AppleSignIn;
+            SignInWithApple();
+        }
+
+        private void SignInWithApple()
+        {
+            Log.ILog.Debug($"apple SignInWithApple");
+
+           var loginArgs = new AppleAuthLoginArgs(LoginOptions.IncludeEmail | LoginOptions.IncludeFullName);
+            AppleAuthManager appleAuthManager = GameObject.Find("Global").GetComponent<Init>().appleAuthManager;
+
+            appleAuthManager.LoginWithAppleId(
+            loginArgs,
+            credential =>
+            {
+                // If a sign in with apple succeeds, we should have obtained the credential with the user id, name, and email, save it
+                //PlayerPrefs.SetString(AppleUserIdKey, credential.User);
+                //this.SetupGameMenu(credential.User, credential);
+                Log.ILog.Debug($"apple登陆成功！！{credential.User}  {credential.ToString()}");
+            },
+            error =>
+            {
+                // var authorizationErrorCode = error.GetAuthorizationErrorCode();
+                //  Debug.LogWarning("Sign in with Apple failed " + authorizationErrorCode.ToString() + " " + error.ToString());
+                // this.SetupLoginMenuForSignInWithApple();
+                Log.Error("apple登陆失败！！");
+            });
+        }
+    }
+}
