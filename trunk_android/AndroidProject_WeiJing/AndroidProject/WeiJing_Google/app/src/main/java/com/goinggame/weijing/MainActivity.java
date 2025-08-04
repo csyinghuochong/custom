8b4eecfbf9afd647cbbb8fc9aa3f915375d0b896
tangchunguang <394266315@qq.com>
2025-08-04 16:05:05 +0800

    no message

--- trunk_android/AndroidProject_WeiJing/AndroidProject/WeiJing_Google/app/src/main/java/com/goinggame/weijing/MainActivity.java
+++ trunk_android/AndroidProject_WeiJing/AndroidProject/WeiJing_Google/app/src/main/java/com/goinggame/weijing/MainActivity.java
@@ -665,27 +665,6 @@ public class MainActivity extends UnityPlayerActivity  implements IIdentifierLis
         }
     }
 
-     /**
-     * 检查手机上是否安装了指定的软件
-     * @param context
-     * @param
-     * @return
-     */
-    public static boolean isAvilible(Context context, String packageName) {
-        final PackageManager packageManager = context.getPackageManager();
-        List<PackageInfo> packageInfos = packageManager.getInstalledPackages(0);
-        List<String> packageNames = new ArrayList<String>();
-
-        if (packageInfos != null) {
-            for (int i = 0; i < packageInfos.size(); i++) {
-                String packName = packageInfos.get(i).packageName;
-                packageNames.add(packName);
-            }
-        }
-        // 判断packageNames中是否有目标程序的包名，有TRUE，没有FALSE
-        return packageNames.contains(packageName);
-    }
-
     static String BuildTransaction(final String type) {
         return (type == null) ? String.valueOf(System.currentTimeMillis()) : type + System.currentTimeMillis();
     }
